e3b5aa8db2c869a3ab6451a7b38368ad65ca596a
dsk <dsk@localhost.localdomain>
2020-04-13 01:48:10 +0200

    heroku sync

--- src/Utils/Sockets.js
+++ src/Utils/Sockets.js
@@ -1,2 +1,2 @@
 import io from 'socket.io-client';
-export default io('http://localhost:8000/sockets',{path:"/sockets"});
+export default io('https://uvsq-bataille-navale-back.herokuapp.com/sockets',{path:"/sockets"});
