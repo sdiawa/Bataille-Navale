59b9c6305a0c9358e09a78bc10c7026a7e7912d3
dsk <dsk@localhost.localdomain>
2020-04-19 09:51:32 +0200

    joueurs en ligne

--- src/Utils/Sockets.js
+++ src/Utils/Sockets.js
@@ -1,3 +1,3 @@
 import io from 'socket.io-client';
 //http://localhost:8000
-export default io('https://uvsq-bataille-navale-back.herokuapp.com', {path: "/sockets"});
+export default io('https://uvsq-bataille-navale-back.herokuapp.com/sockets', {path: "/sockets"});
